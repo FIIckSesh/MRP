d44e7ba1f355ec608051ad6c4f362ade74e4f88c
IgorMihailov <45125141+IgorMihailov@users.noreply.github.com>
2021-02-14 23:27:29 +0300

    create product ui

--- product.py
+++ product.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import sys
 from PyQt5 import QtWidgets
-import ProductUi
+import product_ui
 import re
 from accessify import protected
 
-class ProductUi(QtWidgets.QMainWindow, design.Ui_MainWindow):
+class ProductUi(QtWidgets.QMainWindow, product_ui.Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
