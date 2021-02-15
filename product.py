7ce1bce3f06be8f07da2e3890532bdd95fcd796f
IgorMihailov <45125141+IgorMihailov@users.noreply.github.com>
2021-02-15 09:25:20 +0300

    form for add product

--- product.py
+++ product.py
@@ -7,18 +7,22 @@ import re
 from accessify import protected
 
 class ProductUi(QtWidgets.QMainWindow, product_ui.Ui_MainWindow):
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.prodAddBtn.clicked.connect(self.browse_folder)
+
+    def addProduct(self):
+        newClient = Client();
+
 
 class Product():
-
     def __init__(self, name, price, producer, measurment):
         self.name = name;
         self.price = price;
         self.producer = producer;
         self.measurment = measurment;
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
